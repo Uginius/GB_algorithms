2e4b150bb794200407db81c9d4d54c3c5dda34aa
Uginius <e.slastin@gmail.com>
2019-07-05 14:00:59 +0300

    01

--- src/Lesson2/Study.java
+++ src/Lesson2/Study.java
@@ -1,4 +1,15 @@
 package Lesson2;
 
+import java.util.Random;
+
 public class Study {
+    public static void main(String[] args) {
+        int n = 30;
+        Random rand = new Random();
+        int[] studyArray = new int[n];
+        for (int i = 0; i < n; i++) {
+            studyArray[i] = rand.nextInt(500);
+//            System.out.print(studyArray[i] + "  ");
+        }
+    }
 }
--- src/Lesson2/StudyArrayList.java
+++ src/Lesson2/StudyArrayList.java
@@ -1,4 +1,33 @@
 package Lesson2;
 
-public class StudyArrayList {
+public class StudyArrayList<Item> {
+    private int size = 0;
+    private Object[] list = new Object[1];
+
+    public int getSize() {
+        return size;
+    }
+
+    public Item[] get(int index) {
+        if (index < 0 || index > size - 1) throw new IndexOutOfBoundsException();
+        return (Item[]) list[index];
+    }
+
+    public void set(int index, Item item) {
+        if (index < 0 || index > size - 1) throw new IndexOutOfBoundsException();
+        this.list[index] = item;
+    }
+
+    public void add(Item item) {
+        if (size == list.length) resize(2 * list.length);
+        list[size] = item;
+        size++;
+    }
+
+    private void resize(int capasity) {
+        Object[] temp = new Object[capasity];
+        for (int i = 0; i < size; i++) temp[i] = list[i];
+        list = temp;
+    }
+
 }
