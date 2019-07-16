734fb2f8fb77c949908c159d304e0ff5a4b82661
Uginius <e.slastin@gmail.com>
2019-07-16 14:50:52 +0300

    Lesson 4 optimized (185 lines)

---
+++ src/Lesson4/MyLinkedQueue.java
@@ -0,0 +1,15 @@
+package Lesson4;
+
+import Lesson4.MyLinkedList;
+
+public class MyLinkedQueue<Item> {
+    private MyLinkedList<Item> queue = new MyLinkedList<>();
+
+    public int size() { return queue.size(); }
+
+    public boolean isEmpty() {return queue.isEmpty(); }
+
+    public void enqueue(Item item) { queue.addLast(item); }
+
+    public Item dequeue() { return queue.removeFirst(); }
+}
